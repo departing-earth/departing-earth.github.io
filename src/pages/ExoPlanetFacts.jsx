b2c5cdd8f8ab194b75af20fe30c729a0bb1c66bd
agu02 <107276873+agu02@users.noreply.github.com>
2024-10-06 16:39:00 -0400

    Error page and worm hole css

--- src/pages/ExoPlanetFacts.jsx
+++ src/pages/ExoPlanetFacts.jsx
@@ -24,12 +24,14 @@ const FactsPage = () => {
     // Show loading text if no data is received
     if (!exoplanetData) {
         return (
-            <div className="error-message">
-                <p>Error: Unable to load exoplanet data. Please try again.</p>
-                {/* Button to go back two pages */}
-                <button onClick={() => history.go(-3)} className="retry-button">
-                    Go to previous exoplanet
-                </button>
+            <div className="MainErrorContainer">
+                <div className="error-message">
+                    <p>Error: Unable to load exoplanet data. Please try again.</p>
+                    {/* Button to go back two pages */}
+                    <button onClick={() => history.go(-3)} className="retry-button">
+                        Go to previous exoplanet
+                    </button>
+                </div>
             </div>
         );
     }
